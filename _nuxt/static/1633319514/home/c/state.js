a6f4228e5f216749533d12cd31b7c7241d8e79b1
f-zaryab <zaryabfarooqui@gmail.com>
2021-10-04 08:51:59 +0500

    Updates

---
+++ _nuxt/static/1633319514/home/c/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FnuxtBNB\u002F_nuxt\u002Fstatic\u002F1633319514",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome\u002Fc",config:{_app:{basePath:"\u002FnuxtBNB\u002F",assetsPath:"\u002FnuxtBNB\u002F_nuxt\u002F",cdnURL:a}}}}(null));
